041c19f37849081e7cc402aa3953fbee1fdb26f9
aleangelozi <aleangelozi@gmail.com>
2022-04-20 21:07:45 -0300

    Creating Observables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,41 @@
 import io.reactivex.Observable;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        Observable.just(1,2,3,4,5)
-              .subscribe(System.out::println);
+//        createObservableFromIterable();
+//        createObservableUsingCreate();
+        createObservableWithJust();
 
 
+    }
+
+    private static void createObservableUsingCreate() {
+        Observable<Integer>  observable = Observable.just(1,2,3,4,5);
+        observable.subscribe(item -> System.out.println(item));
+    }
+
+    private static void createObservableFromIterable() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        Observable<Integer> observable = Observable.fromIterable(list);
+        observable.subscribe(System.out::println);
+    }
+
+    private static void createObservableWithJust() {
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(1);
+            emitter.onNext(1);
+            emitter.onNext(1);
+            emitter.onNext(1);
+            emitter.onNext(null);
+            emitter.onComplete();
+        });
+        observable.subscribe(item -> System.out.println(item),
+                error -> System.out.println("There was an error here:" + error.getLocalizedMessage()),
+                () -> System.out.println("Completed"));
     }
 }
