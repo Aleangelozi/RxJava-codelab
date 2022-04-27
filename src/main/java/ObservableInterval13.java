444d969ee80b994ba5ee5549d7087f930dfbea79
aleangelozi <aleangelozi@gmail.com>
2022-04-27 20:25:55 -0300

    Observable interval

---
+++ src/main/java/ObservableInterval13.java
@@ -0,0 +1,23 @@
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
+public class ObservableInterval13 {
+    public static void main(String[] args) {
+        Observable observable = Observable.interval(1, TimeUnit.SECONDS);
+        observable.subscribe(item -> System.out.println("Observer 1: " + item));
+        pause(2000);
+        observable.subscribe(item -> System.out.println("Observer 2: " + item));
+        pause(3000);
+    }
+
+    private static void pause(int duration) {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
