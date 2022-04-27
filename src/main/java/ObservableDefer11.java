ac8675e583c0c8585eadcc87003defa4e11e3fbd
aleangelozi <aleangelozi@gmail.com>
2022-04-27 18:25:48 -0300

    Observable Defer

---
+++ src/main/java/ObservableDefer11.java
@@ -0,0 +1,18 @@
+import io.reactivex.Observable;
+
+public class ObservableDefer11 {
+
+    private static int start = 5, count = 2;
+
+    public static void main(String[] args) {
+
+        // It prints from start to (start + count - 1)
+        Observable<Integer> observable = Observable.defer(() -> {
+            System.out.println("New Observable is created with start = " + start + " and count = " + count);
+            return Observable.range(start, count);
+        });
+        observable.subscribe(item -> System.out.println("Observer 1: " + item));
+        count = 3;
+        observable.subscribe(item -> System.out.println("Observer 2: " + item));
+    }
+}
