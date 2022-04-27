411e7a58f72cd3bc06a40ea12d56b066e580ee70
aleangelozi <aleangelozi@gmail.com>
2022-04-27 18:13:12 -0300

    Observable Range

---
+++ src/main/java/ObservableRange10.java
@@ -0,0 +1,12 @@
+import io.reactivex.Observable;
+
+public class ObservableRange10 {
+    public static void main(String[] args) {
+//        Observable<Integer> observable = Observable.range(0,10);
+//        observable.subscribe(System.out::println);
+
+        int start = 5, count = 2;
+        Observable<Integer> observable = Observable.range(start, count);
+        observable.subscribe(System.out::println);
+    }
+}
