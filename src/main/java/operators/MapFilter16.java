13844a7191dae513f287bf0720754939b241cca3
aleangelozi <aleangelozi@gmail.com>
2022-04-28 11:34:41 -0300

    Map and Filter operators

---
+++ src/main/java/operators/MapFilter16.java
@@ -0,0 +1,41 @@
+package operators;
+
+import io.reactivex.Observable;
+
+public class MapFilter16 {
+    public static void main(String[] args) {
+        //mapOperator();
+        //mapOperatorReturnsDifferentData();
+        //filterOperator();
+        combineMapAndFilter();
+    }
+
+    private static void mapOperator() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        observable
+                .map(item -> item * 2)
+                .subscribe(System.out::println);
+    }
+
+    private static void mapOperatorReturnsDifferentData() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        observable
+                .map(item -> "Hello World!")
+                .subscribe(System.out::println);
+    }
+
+    private static void filterOperator() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        observable
+                .filter(item -> item % 2 == 0)
+                .subscribe(System.out::println);
+    }
+
+    private static void combineMapAndFilter() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+        observable
+                .filter(item -> item % 2 == 0)
+                .map(item -> item * 2)
+                .subscribe(System.out::println);
+    }
+}
